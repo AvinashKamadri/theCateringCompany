fef8511f8ca37fd50731c6f2c94084a47fa0549f
avikkk19 <avinashkamadri14@gmail.com>
2026-03-25 03:56:10 +0530

    fix:c cors issue ml agent

--- ml-agent/api.py
+++ ml-agent/api.py
@@ -46,9 +46,12 @@ async def get_version():
     """Check which code version the server is running."""
     return {"version": _CODE_VERSION}
 
+import os
+_cors_origins = os.environ.get("CORS_ORIGIN", "http://localhost:3000").split(",")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=_cors_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
